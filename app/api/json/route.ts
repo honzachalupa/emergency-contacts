ba709d7b9323ffef9559cb1a03a5244564a629eb
Jan Chalupa <janchalupa@outlook.cz>
2024-02-28 14:23:53 +0100

    WIP.

--- app/api/json/route.ts
+++ app/api/json/route.ts
@@ -1,5 +1,15 @@
 import data from "@/context/data";
+import { NextRequest } from "next/server";
 
-export async function GET() {
-  return Response.json(data);
+export async function GET(request: NextRequest) {
+  const url = new URL(request.url);
+  const apiKey = url.searchParams.get("apiKey");
+
+  if (apiKey === process.env.API_KEY) {
+    return Response.json(data);
+  }
+
+  return new Response(null, {
+    status: 401,
+  });
 }
